e72a70dd670fc4fbb29b8cd0450eab0d33bf3261
P1 <jack@garzafamily.org>
2022-02-07 09:37:24 -0800

    Changes

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
 	
+	
+	//Scroll back to top button w/ relevant animation
+	var btn = $("#toTop");
+
+	$(window).scroll(function() {
+		if ($(window).scrollTop() >= 150) {
+			btn.addClass("show");
+		} 
+		else {
+			btn.removeClass("show");
+		}
+	});
+
+	btn.on("click", function(e) {
+		e.preventDefault();
+		$("html, body").animate({scrollTop:0}, "300");
+	});
 });
